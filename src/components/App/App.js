e984a9715c9d38788deffbcc6c9c30eea0691b20
Evgeniy Karmyskov <karmyskove@gmail.com>
2021-12-13 18:17:37 +0700

    refactor: decomposed code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,57 +1,20 @@
-import React from 'react';
-import './App.css';
-import logo from '../../images/logo.svg';
-import avatar from '../../images/avatar.jpg';
+import React from "react";
+import "./App.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Profile from "../Profile/Profile";
+import Places from "../Places/Places";
 
 const App = () => {
     return (
         <div className="body">
             <div className="page">
-                <header className="header page__header">
-                    <a href="#">
-                        <img
-                            className="header__logo"
-                            src={logo}
-                            alt="Логотип веб-сайта Место"
-                        />
-                    </a>
-                </header>
-
+                <Header />
                 <main className="content">
-
-                    <section className="profile page__profile">
-                        <div className="profile__wrap">
-                            <div className="profile__inner-edit">
-                                <img
-                                    className="profile__avatar"
-                                    src={avatar}
-                                    alt="Аватар пользователя"
-                                />
-                                <button className="profile__btn-avatar" type="button"></button>
-                            </div>
-                            <div className="profile__inner">
-                                <div className="profile__row">
-                                    <h1 className="profile__name"></h1>
-                                    <button
-                                        className="profile__edit"
-                                        type="button"
-                                        aria-label="Изменить">
-                                    </button>
-                                </div>
-                                <p className="profile__description"></p>
-                            </div>
-                        </div>
-                        <button className="profile__btn" type="button" aria-label="Добавить"></button>
-                    </section>
-
-                    <section className="places page__places">
-                        <ul className="places__cards"></ul>
-                    </section>
+                    <Profile />
+                    <Places />
                 </main>
-
-                <footer className="footer">
-                    <p className="footer__copyright">2021 Mesto Russia</p>
-                </footer>
+                <Footer />
             </div>
         </div>
     )
